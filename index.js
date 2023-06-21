9fca3a7e7eb0d88dfc622380f3d8dc88301a6f28
veselingifted <136447285+veselingifted@users.noreply.github.com>
2023-06-21 19:51:18 +0300

    search input

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const searchBtn = document.getElementById("glow-on-hover");
-const searchBar = document.getElementById("search");
-
-let searchQuery;
-
-searchBar.addEventListener("keydown", (e) => {
-    if (e.key == "Enter" && searchBar.value != "") {
-        location.href = 'pages/results/results.html';
-        searchQuery = searchBar.value;
-        console.log(searchQuery)
-    }
+function handleSearch() {
+    let searchInput = document.getElementById(`search-input`).value;
+    console.log("Search Input:",searchInput);
 }
